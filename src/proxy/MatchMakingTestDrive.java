923dae06867d2ed87a079f1296a55864d25c02fc
zhangtao <zhangtao@anji-plus.com>
2018-08-19 15:58:13 +0800

    代理模式

---
+++ src/proxy/MatchMakingTestDrive.java
@@ -0,0 +1,56 @@
+package proxy;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 2018/8/19
+ * Time: 0:44
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class MatchMakingTestDrive {
+    public static void main(String[] args) {
+        MatchMakingTestDrive matchMakingTestDrive = new MatchMakingTestDrive();
+        matchMakingTestDrive.driver();
+    }
+
+
+    public void driver() {
+        PersonBean joe = new PersonBeanImpl("joe", "joeGender", "bowling", 2, 10);
+
+        //获取代理对象proxy
+        PersonBean ownerProxy = getOwnerProxy(joe);
+        //调用代理对象的方法--》会调用处理器invocationHandler的invoke()方法
+        System.out.println("name is " + ownerProxy.getName());
+        ownerProxy.setInterests("bowling,go");
+        System.out.println("interests set from ownerProxy");
+        try {
+            ownerProxy.setHotOrNotRating(8);
+        } catch (Exception e) {
+            System.out.println("can not hotOrNotRating set from ownerProxy");
+        }
+        System.out.println("Rating is " + ownerProxy.getHotOrNotRating());
+
+
+        PersonBean nonOwnerProxy = getNonOwnerProxy(joe);
+        try {
+            nonOwnerProxy.setInterests("gogo");
+        } catch (Exception e) {
+            System.out.println("can not interests set from nonOwnerProxy");
+        }
+        nonOwnerProxy.setHotOrNotRating(8);
+        System.out.println("Rating is " + nonOwnerProxy.getHotOrNotRating());
+    }
+
+
+    PersonBean getOwnerProxy(PersonBean person) {
+        return (PersonBean) Proxy.newProxyInstance(person.getClass().getClassLoader(), person.getClass().getInterfaces(), new OwnerInvocationHandler(person));
+    }
+
+
+    PersonBean getNonOwnerProxy(PersonBean person) {
+        return (PersonBean) Proxy.newProxyInstance(person.getClass().getClassLoader(), person.getClass().getInterfaces(), new NonOwnerInvocationHandler(person));
+    }
+}
---
+++ src/proxy/NonOwnerInvocationHandler.java
@@ -0,0 +1,38 @@
+package proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 2018/8/19
+ * Time: 0:20
+ * To change this template use File | Settings | File Templates.
+ * Description:看别人时，只能查看和打分，不能修改别人的信息
+ */
+public class NonOwnerInvocationHandler implements InvocationHandler {
+
+    PersonBean person;
+
+    public NonOwnerInvocationHandler(PersonBean person) {
+        this.person = person;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        try {
+            if (method.getName().startsWith("get")) {
+                return method.invoke(person, args);
+            } else if (method.getName().equals("setHotOrNotRating")) {
+                return method.invoke(person, args);
+            } else if (method.getName().startsWith("set")) {
+                throw new IllegalAccessException();
+            }
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
