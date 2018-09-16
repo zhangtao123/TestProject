2d1bb3042adce2184402353129afc23e925d149e
zhangtao <zhangtao@anji-plus.com>
2018-09-16 20:08:46 +0800

    提交代码

--- src/TestMap.java
+++ src/TestMap.java
@@ -33,8 +33,36 @@ public class TestMap {
     }
 
 
+    public static void do2(){
+        Map<Integer,String> map = new HashMap<Integer,String>();
+        map.put(3,"111");
+
+
+
+        Integer in = map.keySet().iterator().next();
+        System.out.println(in);
+
+        System.out.println(map.size());
+    }
+
+
+
+    public static void do3(){
+        Map<String, Integer> map = new HashMap();
+        map.put("1", 8);
+        map.put("2", 12);
+        map.put("3", 53);
+        map.put("4", 33);
+        map.put("5", 11);
+        map.put("6", 3);
+        map.put("7", 3);
+        List<Map.Entry<String,Integer>> list = new ArrayList(map.entrySet());
+        Collections.sort(list, (o1, o2) -> (o2.getValue() - o1.getValue()));
+        System.out.println(list.get(0).getKey());
+        System.out.println(list.get(1).getKey());
+    }
 
     public static void main(String[] args) {
-        System.out.println("test");
+        do3();
     }
 }
