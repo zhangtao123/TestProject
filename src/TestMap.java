6f5b0b0583068fedf99550f2580b9256078cf168
zhangtao <zhangtao@anji-plus.com>
2018-12-07 15:03:21 +0800

    提交代码

--- src/TestMap.java
+++ src/TestMap.java
@@ -17,8 +17,7 @@ import java.util.stream.Collectors;
 public class TestMap {
 
 
-
-    public static void do1(){
+    public static void do1() {
 //        Map<String,Object> map1 = new ConcurrentHashMap<String, Object>();
 //
 //        Map<String,Object> map2 = Collections.synchronizedMap();
@@ -37,10 +36,9 @@ public class TestMap {
     }
 
 
-    public static void do2(){
-        Map<Integer,String> map = new HashMap<Integer,String>();
-        map.put(3,"111");
-
+    public static void do2() {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(3, "111");
 
 
         Integer in = map.keySet().iterator().next();
@@ -50,8 +48,7 @@ public class TestMap {
     }
 
 
-
-    public static void do3(){
+    public static void do3() {
         Map<String, Integer> map = new HashMap();
         map.put("1", 8);
         map.put("2", 12);
@@ -60,19 +57,18 @@ public class TestMap {
         map.put("5", 11);
         map.put("6", 3);
         map.put("7", 3);
-        List<Map.Entry<String,Integer>> list = new ArrayList(map.entrySet());
+        List<Map.Entry<String, Integer>> list = new ArrayList(map.entrySet());
         Collections.sort(list, (o1, o2) -> (o2.getValue() - o1.getValue()));
         System.out.println(list.get(0).getKey());
         System.out.println(list.get(1).getKey());
     }
 
 
-
-    public static void do4(){
+    public static void do4() {
         Map<String, Integer> map = new IdentityHashMap();
-        for (int i= 0;i<4;i++){
-            map.put(String.valueOf(i),i+i);
-            map.put(String.valueOf(i),i+i);
+        for (int i = 0; i < 4; i++) {
+            map.put(String.valueOf(i), i + i);
+            map.put(String.valueOf(i), i + i);
         }
         Set<Map.Entry<String, Integer>> set = map.entrySet();
         System.out.println(set);
@@ -87,14 +83,14 @@ public class TestMap {
     }
 
 
-    public static void t1(){
+    public static void t1() {
         System.out.println("11111111");
         String rs = t2();
         System.out.println("22222222");
     }
 
-    public static String t2(){
-        int i = 5/0;
+    public static String t2() {
+        int i = 5 / 0;
         return "11";
     }
 
@@ -142,8 +138,7 @@ public class TestMap {
     }
 
 
-
-    public static String do6(){
+    public static String do6() {
         Person p1 = new Person("", 12);
         Person p2 = new Person("ww", 13);
         Person p3 = new Person("ww", 14);
@@ -156,7 +151,7 @@ public class TestMap {
         list.add(p4);
         list.add(p5);
         Map<String, List<Person>> personGroups = list.stream().collect(Collectors.groupingBy(Person::getName));
-        personGroups.forEach((a,b)-> System.out.println(a+";"+b));
+        personGroups.forEach((a, b) -> System.out.println(a + ";" + b));
         ConcurrentHashMap con = new ConcurrentHashMap();
         Hashtable tab = new Hashtable();
 
@@ -164,32 +159,72 @@ public class TestMap {
     }
 
 
-    public static void do7(){
+    public static void do7() {
         int n = 1 << 30;
         System.out.println(n);
     }
 
     final Object[] items;
+
     public TestMap() {
         this.items = new Object[8];
     }
 
 
-
-    public void do8(){
+    public void do8() {
         ArrayBlockingQueue array = new ArrayBlockingQueue(10);
         array.peek();
         array.remove();
     }
 
     //+43  -45  48~57
-    public static void do9(){
+    public static void do9() {
         char c = '9';
         System.out.println((int) c);
     }
 
 
+    public static void do10(int n) {
+        if (n == 6) {
+            System.out.println(n);
+        } else if (n > 2) {
+            System.out.println(n);
+        } else {
+            System.out.println(n);
+        }
+    }
+
+
+    public static void do11() {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(6);
+        list.add(3);
+        list.add(88);
+        list.add(16);
+        list.sort(Comparator.comparingInt(o -> o.intValue()));
+        //list.sort((o1, o2) -> (o1.intValue() - o2.intValue()));
+        System.out.println(list);
+
+    }
+
+
+    public static void do12() {
+        String str = "1,6,3,88,16";
+        String[] arr = str.split(",");
+        List<Integer> list = new ArrayList<>();
+        for (String str1 : arr) {
+            list.add(Integer.valueOf(str1));
+        }
+        //System.out.println(list.get(0).getClass().getName());
+        System.out.println(list);
+        list.remove(Integer.valueOf(6));
+        list.remove(Integer.valueOf(88));
+        System.out.println(list);
+    }
+
+
     public static void main(String[] args) {
-        do9();
+        do12();
     }
 }
